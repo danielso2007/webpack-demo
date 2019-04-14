020a24e3ae1e4474c65d0ab018f18e0225cf38dd
danielso2007 <danielso2007@gmail.com>
2019-04-14 02:36:22 -0300

    feat(Limpar a dist): Gerenciamento de saída.

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # Estudo de webpack
 
+# Configurando o HtmlWebpackPlugin
+
+HtmlWebpackPlugin por padrão, ela gerará seu próprio index.html arquivo, mesmo que já tenhamos um na dist/pasta.
+
+Se você quiser saber mais sobre todos os recursos e opções que [HtmlWebpackPlugin](https://github.com/jantimon/html-webpack-plugin) oferece, então você deve ler sobre o HtmlWebpackPluginrepo.
+
+Você também pode dar uma olhada no [html-webpack-template](https://github.com/jaketrent/html-webpack-template) que fornece alguns recursos extras além do modelo padrão.
+
+```shell
+npm install --save-dev html-webpack-plugin
+```
+
+# Limpar a /dist
+
+Um plugin popular para gerenciar isso é o mesmo, [clean-webpack-plugin](https://www.npmjs.com/package/clean-webpack-plugin).
+
+```shell
+npm install --save-dev clean-webpack-plugin
+```
+
+
 # Gerando o CHANGELOG do projeto
 
 ```shell
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,52 @@
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// HtmlWebpackPluginpor padrão, ela gerará seu próprio index.html, mesmo que já tenhamos um na dist/.
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-function createWebpackConfig() {
+function createModules() {
+    let ruleCSS = {test: /\.css$/, use: ['style-loader', 'css-loader']};
+    let ruleImage = {test: /\.(png|svg|jpg|gif)$/, use: [{loader: 'file-loader', options: { name: '[name].[ext]', outputPath: 'images/' }}]};
+    let ruleFonts = {test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/, use: [{loader: 'file-loader', options: { name: '[name].[ext]', outputPath: 'fonts/' }}]};
+    return {rules: [
+        ruleCSS, 
+        ruleImage, 
+        ruleFonts]};
+}
+
+function createPlugins() {
+    let cleanWebpackPlugin = new CleanWebpackPlugin();
+    let miniCssExtractPlugin = new MiniCssExtractPlugin({filename: "[name].css", chunkFilename: "[id].css"});
+    let webpackEnvironmentPlugin = new webpack.EnvironmentPlugin({
+        NODE_ENV: 'development', 
+        DEBUG: false});
+    let webpackDefinePlugin = new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'process.env.DEBUG': JSON.stringify(process.env.DEBUG)
+    });
+    let htmlWebpackPlugin = new HtmlWebpackPlugin({
+        'title': 'Output Management',
+        'meta' : {
+            viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+            'application-name': 'Estudo Webpack',
+            'theme-color': '#4285f4',
+            'robots': 'index,follow',
+            'googlebot': 'index,follow'
+        }
+    });
+    return [
+        cleanWebpackPlugin,
+        miniCssExtractPlugin, 
+        webpackEnvironmentPlugin, 
+        webpackDefinePlugin,
+        htmlWebpackPlugin];
+}
+
+function createWebpackConfig(mode) {
     let webpackConfig = {};
 
-    webpackConfig.mode = 'development';
+    webpackConfig.mode = mode;
     webpackConfig.entry = {
         app: './src/index.js',
         print: './src/print.js'
@@ -14,34 +55,27 @@ function createWebpackConfig() {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     };
-    let ruleCSS = {test: /\.css$/, use: ['style-loader', 'css-loader']};
-    let ruleImage = {test: /\.(png|svg|jpg|gif)$/, use: [{loader: 'file-loader', options: { name: '[name].[ext]', outputPath: 'images/' }}]};
-    let ruleFonts = {test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/, use: [{loader: 'file-loader', options: { name: '[name].[ext]', outputPath: 'fonts/' }}]};
-    webpackConfig.module = {rules: [ruleCSS, ruleImage, ruleFonts]};
 
-    let miniCssExtractPlugin = new MiniCssExtractPlugin({filename: "[name].css", chunkFilename: "[id].css"});
-    let webpackEnvironmentPlugin = new webpack.EnvironmentPlugin({NODE_ENV: 'development', DEBUG: false});
-    let webpackDefinePlugin = new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-        'process.env.DEBUG': JSON.stringify(process.env.DEBUG)
-    });
-    webpackConfig.plugins = [miniCssExtractPlugin, webpackEnvironmentPlugin, webpackDefinePlugin];
+    webpackConfig.module = createModules();
+    webpackConfig.plugins = createPlugins();
+
+    if (mode === 'development') {
+        webpackConfig.devtool = 'source-map';
+    }
 
     return webpackConfig;
 }
 
 module.exports = (env, argv) => {
-    let config = createWebpackConfig();
+    let mode = argv.production ? 'production' : 'development';
 
-    if (argv.mode === 'development') {
-        config.devtool = 'source-map';
-    }
-
-    if (argv.mode === 'production') {
+    if (mode === 'production' || argv.production) {
         console.log('Executando build para produção.');
     } else {
         console.log('Executando build para desenvolvimento.');
     }
+    
+    let config = createWebpackConfig(mode);
 
     return config;
 };
