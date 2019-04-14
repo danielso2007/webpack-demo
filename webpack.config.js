0aedfcaa4e470e7a7112a5206c53acabd2493be8
danielso2007 <danielso2007@gmail.com>
2019-04-14 03:53:10 -0300

    feat: MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const webpack = require('webpack');
 const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// HtmlWebpackPluginpor padrão, ela gerará seu próprio index.html, mesmo que já tenhamos um na dist/.
+
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-function createModules() {
-    let ruleCSS = {test: /\.css$/, use: ['style-loader', 'css-loader']};
+function createModules(devMode) {
+    let ruleCSS = {test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: devMode,
+              reloadAll: true,
+            },
+          },
+          'css-loader',
+    ]};
     let ruleImage = {test: /\.(png|svg|jpg|gif)$/, use: [{loader: 'file-loader', options: { name: '[name].[ext]', outputPath: 'images/' }}]};
     let ruleFonts = {test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/, use: [{loader: 'file-loader', options: { name: '[name].[ext]', outputPath: 'fonts/' }}]};
     return {rules: [
@@ -15,9 +27,12 @@ function createModules() {
         ruleFonts]};
 }
 
-function createPlugins() {
+function createPlugins(devMode) {
     let cleanWebpackPlugin = new CleanWebpackPlugin();
-    let miniCssExtractPlugin = new MiniCssExtractPlugin({filename: "[name].css", chunkFilename: "[id].css"});
+    let miniCssExtractPlugin = new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    });
     let webpackEnvironmentPlugin = new webpack.EnvironmentPlugin({
         NODE_ENV: 'development', 
         DEBUG: false});
@@ -43,21 +58,24 @@ function createPlugins() {
         htmlWebpackPlugin];
 }
 
-function createWebpackConfig(mode) {
-    let webpackConfig = {};
+function createServer() {
+    return {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        open: false,
+        watchContentBase: true,
+        clientLogLevel: 'info',
+        port: 8080,
+        after: function(app, server) {
+        },
+        before: function(app, server) {
+        }
+    }
+}
 
-    webpackConfig.mode = mode;
-    webpackConfig.entry = {
-        app: './src/index.js',
-        print: './src/print.js',
-        another: './src/another-module.js'
-    };
-    webpackConfig.output = {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    };
-
-    webpackConfig.optimization = {
+function createOptimization(devMode) {
+    return {
+        minimizer: [new OptimizeCSSAssetsPlugin({})],
         splitChunks: {
             chunks: 'all',
             minSize: 30000,
@@ -65,11 +83,11 @@ function createWebpackConfig(mode) {
             minChunks: 1,
             maxAsyncRequests: 5,
             maxInitialRequests: 3,
-            automaticNameDelimiter: '_',
+            automaticNameDelimiter: '.',
             name: true,
             cacheGroups: {
                 vendors: {
-                    filename: '[name].bundle.js',
+                    filename: devMode ? '[name].bundle.js' : '[name].[hash].bundle.js',
                     test: /[\\/]node_modules[\\/]/,
                     enforce: true,
                     priority: -10
@@ -82,26 +100,31 @@ function createWebpackConfig(mode) {
             }
         }
     };
+}
 
-    webpackConfig.module = createModules();
-    webpackConfig.plugins = createPlugins();
+function createWebpackConfig(devMode) {
+    let webpackConfig = {};
 
-    if (mode === 'development') {
+    webpackConfig.entry = {
+        app: './src/index.js',
+        print: './src/print.js',
+        another: './src/another-module.js'
+    };
+    webpackConfig.output = {
+        filename: devMode ? '[name].bundle.js' : '[name].[hash].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    };
+
+    webpackConfig.optimization = createOptimization(devMode);
+
+    if (devMode) {
         webpackConfig.devtool = 'source-map';
     }
+
+    webpackConfig.module = createModules(devMode);
+    webpackConfig.plugins = createPlugins(devMode); 
     
-    webpackConfig.devServer = {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        open: false,
-        watchContentBase: true,
-        clientLogLevel: 'info',
-        port: 8080,
-        after: function(app, server) {
-        },
-        before: function(app, server) {
-        }
-    }
+    webpackConfig.devServer = createServer();
 
     return webpackConfig;
 }
@@ -115,7 +138,8 @@ module.exports = (env, argv) => {
         console.log('Executando build para desenvolvimento.');
     }
     
-    let config = createWebpackConfig(mode);
+    let config = createWebpackConfig(mode === 'development');
+    config.mode = mode;
 
     return config;
 };
